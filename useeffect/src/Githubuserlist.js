399878695e7b949d1347d8305f04af6db37a0faf
dav1lex <zedxurl@gmail.com@>
2023-02-23 23:04:33 +0100

    I forgot to useeffect so, there you have

--- useeffect/src/Githubuserlist.js
+++ useeffect/src/Githubuserlist.js
@@ -1,38 +1,40 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Showuser} from "./Showuser";
 
 export function Githubuserlist() {
     const [userlist, setuserlist] = useState([])
-    const [username, setusername] = useState('')
+    const [username, setusername] = useState('dav1lex')
+    const [passthis, setpassthis] = useState('')
 
-    function submitUser() {
-
+    useEffect(() => {
         fetch(`https://api.github.com/users/${username}`)
             .then(response => {
-                if(response.status === 200) {
+                if (response.status === 200) {
                     return response.json()
-                }else{
-                   document.getElementById('input1').value = 'not found'
+                } else {
+                    document.getElementById('input1').value = 'not found'
                 }
             })
             .then(json => {
                 setuserlist([...userlist, {avatar_url: json.avatar_url, login: json.login, name: json.name}])
             })
+    }, [username])
 
-        setusername('')
-
+    function submitUser() {
+        setusername(passthis)
+        setpassthis('')
     }
 
     function myHandler(event) {
         event.preventDefault()
         const name = event.target.value;
-        setusername(name)
+        setpassthis(name)
     }
 
     console.log(userlist)
     return (
         <>
-            <input id='input1' value={username} onChange={myHandler}/>
+            <input id='input1' value={passthis} onChange={myHandler}/>
             <button type='submit' onClick={submitUser}>Sear</button>
             <Showuser users={userlist}/>
         </>
